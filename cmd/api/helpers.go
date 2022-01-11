4ce56a1ca7a49bf1c3acda3101d327e542f6051b
atcolab <atcolab00@gmail.com>
2022-01-10 20:17:26 -0500

    sending json responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"errors"
-	"net/http"
-	"strconv"
+    "encoding/json"
+    "errors"
+    "net/http"
+    "strconv"
 
-	"github.com/julienschmidt/httprouter"
+    "github.com/julienschmidt/httprouter"
 )
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
+    params := httprouter.ParamsFromContext(r.Context())
 
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
-	}
+    id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+    if err != nil || id < 1 {
+        return 0, errors.New("invalid id parameter")
+    }
 
-	return id, nil
+    return id, nil
+}
+
+type envelope map[string]interface{}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+    js, err := json.MarshalIndent(data, "", "\t")
+    if err != nil {
+        return err
+    }
+
+    js = append(js, '\n')
+
+    for key, value := range headers {
+        w.Header()[key] = value
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(status)
+    w.Write(js)
+
+    return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,20 +1,36 @@
 package main
 
+
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
+    "time" 
+
+    "greenlight.atcolab/internal/data" // New import
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+    fmt.Fprintln(w, "create a new movie")
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := app.readIDParam(r)
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
+    id, err := app.readIDParam(r)
+    if err != nil {
+        app.notFoundResponse(w, r)
+        return
+    }
 
-	fmt.Fprintf(w, "show the details of the movie %d\n", id)
+    movie := data.Movie{
+        ID:        id,
+        CreatedAt: time.Now(),
+        Title:     "Casablanca",
+        Runtime:   102,
+        Genres:    []string{"drama", "romance", "war"},
+        Version:   1,
+    }
+
+    err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+    }
 }
